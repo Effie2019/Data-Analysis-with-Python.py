7864d417f4d7319fc264633efc7dd50fcee1a741
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2019-10-01 23:49:41 -0400

    Update Import Data.py

--- Import Data.py
+++ Import Data.py
@@ -1,17 +1,16 @@
 # import pandas library
 import pandas as pd
-
 # Read the online file by the URL provides above, and assign it to variable "df", no header
 other_path = "https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/auto.csv"
 df = pd.read_csv(other_path, header=None)
 
 # show the first 5 rows using dataframe.head() method
-print("The first 5 rows of the dataframe") 
 df.head(5)
-
 # Add headers 
 headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
          "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
          "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
          "peak-rpm","city-mpg","highway-mpg","price"]
 df.columns = headers
+#drop missing value
+df.dropna(subset=["price"], axis=0)
